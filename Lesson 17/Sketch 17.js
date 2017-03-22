58b83f83600bcd2bb8b476694bb9a4c8c24f7a4c
Jakebrickmaster <17reed.jacob@providencehigh.org>
2017-03-22 14:30:01 -0700

    yeet

--- Lesson 17/Sketch 17.js
+++ Lesson 17/Sketch 17.js
@@ -6,9 +6,27 @@ var url2 = '/relatedWords?useCanonical=true&limitPerRelationshipType=10&api_key=
 
 function setup(){
     noCanvas();
-    loadJSON(url1+word+url2, gotData);
+   
+    link = createA('#', word);
+    //link.mousePressed(askWordnik);
+    var button = select('#submit');
+    button.mousePressed(askWordnik);
+    input = select('#city');
 }
 
 function gotData(Raemon){
     console.log(Raemon);
+    
+    
+    var index1 = floor(random(0, Raemon.length))
+    var index2 = floor(random(0, Raemon[index1].words.length))
+    
+    word = Raemon[index1].words[index2];
+    console.log(Raemon[index1].words[index2]);
+    link.html(Raemon[index1].words[index2]);
+}
+
+function askWordnik(){
+     loadJSON(url1+word+url2, gotData);
+    
 }
