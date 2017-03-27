b92a97ad5de9cf5c0f6828b63a2d7f8bcfdf8e79
Jakebrickmaster <17reed.jacob@providencehigh.org>
2017-03-27 15:12:56 -0700

    HFKHNFL:S

--- Lesson 18/Sketch 18.js
+++ Lesson 18/Sketch 18.js
@@ -1,6 +1,11 @@
 var apikey =  "?api-key=3a4cb87a9dbd4c4c800759527691892c";
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 var search;
+var begin;
+var end;
+var submit;
+var begindate = "&begin_date=";
+var enddate = "&end_date=";
 var query = "&q=";
 
 //https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3a4cb87a9dbd4c4c800759527691892c&q=instagram
@@ -10,15 +15,24 @@ function setup(){
 
     //link = createA('#', "Click me for Instagram Articles!");
     //link.mousePressed(findArticles);
-    var button = select('#submit');
-    button.mousePressed(findArticles);
-    input = select('#search');
+    search = select('#search');
+    begin = select('#begin');
+    end = select('#end');
+    submit = select('#submit');
+    
+    //var button = select('#submit');
+    submit.mousePressed(findArticles);
+    //input = select('#search');
 }
 
 
 function findArticles(){
-    search = input.value();
-    loadJSON(url+apikey+query+search, gotData);
+   // search = input.value();
+    var s = search.value();
+    var b = begin.value();
+    var e = end.value();
+    
+    loadJSON(url+apikey+query+s+begindate+b+enddate+e, gotData);
 }
 
 function gotData(data){
---
+++ Lesson 19/Sketch19.js
@@ -0,0 +1,9 @@
+/*
+All computers/ laptops run on a P5.JS framework for web browsers.
+Also known as the cloud. Clouds and Servers are almost identical.
+WHen you search something, you send a request to the cloud and it responds back.
+
+OAuth
+*/
+
+console.log("Hello ECS!");
